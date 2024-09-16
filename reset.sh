fe2457e3d75473ab51951913f7832b0a3646e1d6
Jonathan Chappelow <jon@kwil.com>
2024-09-16 18:29:40 -0500

    resetagain

--- reset.sh
+++ reset.sh
@@ -1,6 +1,6 @@
 rm -r {n0,n1,n2,n3,n3x}/{app,kvstore.db}
 rm -r {n0,n1,n2,n3,n3x}/*.db
-rm -r {n0,n1,n2,n3,n3x}/*.wal
+rm -r {n0,n1,n2,n3,n3x}/data/*.wal
 rm -r {n0,n1,n2,n3,n3x}/data/*.db
 echo '{"height": "0","round":0,"step":0}' > n0/data/priv_validator_state.json
 echo '{"height": "0","round":0,"step":0}' > n1/data/priv_validator_state.json
