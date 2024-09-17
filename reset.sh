71d1f8865aba367ec30ccccc62f1c3f67316c255
Jonathan Chappelow <chappjc@protonmail.com>
2024-09-16 21:12:19 -0500

    update .gitignore and reset.sh

--- reset.sh
+++ reset.sh
@@ -1,5 +1,6 @@
-rm -r {n0,n1,n2,n3,n3x}/{app,kvstore.db}
-rm -r {n0,n1,n2,n3,n3x}/*.db
+#!/usr/bin/env bash
+
+rm -r {n0,n1,n2,n3,n3x}/app
 rm -r {n0,n1,n2,n3,n3x}/data/*.wal
 rm -r {n0,n1,n2,n3,n3x}/data/*.db
 echo '{"height": "0","round":0,"step":0}' > n0/data/priv_validator_state.json
