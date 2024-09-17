8a25c276577184fa8f07e69d2e904728a3b68f96
Jonathan Chappelow <chappjc@protonmail.com>
2024-09-16 21:14:20 -0500

    use the kind kvstore

--- app/app.go
+++ app/app.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"path/filepath"
 
-	"github.com/cometbft/cometbft/abci/example/kvstore"
+	// "github.com/cometbft/cometbft/abci/example/kvstore"
 	"github.com/cometbft/cometbft/abci/server"
 	"github.com/cometbft/cometbft/libs/service"
 )
 
 func StartAppServer(ctx context.Context, dbDir, port string) (service.Service, error) {
-	app := kvstore.NewPersistentApplication(filepath.Join(dbDir, "app"))
+	// app := kvstore.NewPersistentApplication(filepath.Join(dbDir, "app"))
+	app := NewPersistentApplication(filepath.Join(dbDir, "app"))
 
 	laddr := "0.0.0.0:" + port
 	server, err := server.NewServer("tcp://"+laddr, "socket", app)
